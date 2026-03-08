0b11031eeda3af6d9cc0ae0a1194fe6ac4f41ec6
Кирик <kmaibo@mail.ru>
2026-03-08 20:57:04 +0300

    Completed

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -4,21 +4,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
 import ru.hogwarts.school.service.StudentService;
 
 import java.net.URI;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
 
     private StudentService studentService;
+    private StudentRepository studentRepository;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, StudentRepository studentRepository) {
         this.studentService = studentService;
+        this.studentRepository = studentRepository;
     }
 
     @PostMapping
@@ -43,7 +45,7 @@ public class StudentController {
 
     @GetMapping
     public ResponseEntity<Collection<Student>> getAllStudent(@RequestParam(required = false) Integer age,
-                                                             @RequestParam (required = false) Integer age2) {
+                                                             @RequestParam(required = false) Integer age2) {
         return ResponseEntity.ok(studentService.getAllStudents());
     }
 
@@ -62,5 +64,40 @@ public class StudentController {
     public ResponseEntity<Faculty> getFacultyById(@PathVariable Long id) {
         Faculty faculty = studentService.getFacultyById(id);
         return ResponseEntity.ok(faculty);
+    }
+
+    @GetMapping("/names-starting-with-a")
+    public List<String> getAllNamesStartingWithA() {
+        return studentService.getAllStudents().stream().map(Student::getName)
+                .filter(name -> name != null && name.toUpperCase().startsWith("A"))
+                .map(String::toUpperCase).sorted().collect(Collectors.toList());
+    }
+
+    @GetMapping("/average-age")
+    public Double getAverageAge() {
+        List<Student> students = studentRepository.findAll();
+        if (students.isEmpty()) {
+            return 0.0;
+        }
+
+        return students.stream().mapToInt(Student::getAge)
+                .average().orElse(0.0);
+    }
+
+    @GetMapping("/longest-faculty-name")
+    public String getLongestFacultyName() {
+        return studentRepository.findAll().stream()
+                .map(Student::getFaculty)
+                .filter(Objects::nonNull)
+                .map(Faculty::getName).
+                filter(name -> name != null && !name.trim().isEmpty())
+                .max(Comparator.comparing(String::length))
+                .orElse("");
+    }
+
+    @GetMapping("/optimized-sum")
+    public long getOptimizedSum() {
+        long n = 1000000L;
+        return n * (n + 1) / 2;
     }
 }
